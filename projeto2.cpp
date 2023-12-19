3cd02f11888e014b875081190032d344c35c3103
luca-dallalana <luca.dallalana@tecnico.ulisboa.pt>
2023-12-19 21:40:28 +0000

    input

--- projeto2.cpp
+++ projeto2.cpp
@@ -8,40 +8,25 @@ using namespace std;
 // g++ -std=c++11 -O3 -Wall projeto2.cpp -lm ./a.out
 
 int DFS(vector<vector<int>>& adjacencyList, int personSearched, int people){
-    int jumps = 0;
-    vector<bool> visited(people + 1, false);   
-    stack<int> stack;
-
-    visited[personSearched] = true;
-    stack.push(personSearched);
-    
-    while (!stack.empty()){
-        int currentPerson = stack.top();
-        stack.pop();
-        for (int knownPerson : adjacencyList[currentPerson]){
-            if (!visited[knownPerson]){
-                visited[knownPerson] = true;
-                stack.push(knownPerson);
-                jumps++;
-            }
-        }
-    }
-    return jumps - 1;
 }
 
 int main(){
     int people, relationships;
     scanf("%d %d", &people, &relationships); 
     vector<vector<int>> adjacencyList(people + 1);
+    vector<vector<int>> adjacencyListTransposta(people + 1);
     int jumps = 0;
-    for (int i = 0; i < relationships; i++){
+    for (int i = 0; i < relationships; i++){ // cria a lista de adjacencia do grafo normal e do grafo transposto (grafo transposto serve pra decobrir SCCs)
         int person1, person2;
         scanf("%i %i", &person1, &person2);
         adjacencyList[person1].push_back(person2);
+        adjacencyListTransposta[person2].push_back(person1);
     }
-    for (int v = 0; v < people; v++){
-        jumps = max(jumps, DFS(adjacencyList, v, people));
+    for (int i = 0; i < people + 1; i++){ // no final de cada adjacencyList adiconar um 0, util para os SCCs
+        adjacencyList[i].push_back(0);
+        adjacencyListTransposta[i].push_back(0);
     }
-        
+    
+
     return 0;
 }
