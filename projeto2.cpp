afd28051f8e604fa0b4ee7be36d64465ffe3c22e
luca-dallalana <luca.dallalana@tecnico.ulisboa.pt>
2023-12-19 22:12:43 +0000

    DFS

--- projeto2.cpp
+++ projeto2.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
-#include <cmath>
 #include <stack>
+#include <list>
 using namespace std;
 
 // g++ -std=c++11 -O3 -Wall projeto2.cpp -lm ./a.out
 
-int DFS(vector<vector<int>>& adjacencyList, int personSearched, int people){
+list<int> DFS(vector<vector<int>>& adjacencyList, int people){
+    vector<string> colors(people +1, "White");
+    list<int> visit;
+    list<int> visited;
+    int currentV;
+    for (int i = 1; i <= people; i++){ // percorre todos os vertices
+        if (colors[i] == "White"){
+            visit.push_front(i); // adiciona a lista dos a procurar
+            while (!visit.empty()){
+                currentV = visit.front();
+                colors[currentV] = "Gray"; // maybe redundante
+                for (int adjacentV : adjacencyList[currentV]){
+                    if (adjacentV != 0){ // Se houver v adjacente
+                        if (colors[adjacentV] == "White"){ // Se ainda nao foi visitado
+                            colors[adjacentV] = "Gray"; // Passa a ser visitado mas nao fechado ainda maybe redundante
+                            visit.push_front(adjacentV);
+                            break; // Para nao procurar pelos outros adjacentes, enquanto nao acaba o "ramo da arvore"
+                        }
+                        
+                    }
+                    else{ // Se nao houver adjacente
+                        visited.push_front(currentV); // Adiciona o vertice a lista dos visitados
+                        visit.pop_front(); // Remove o vertice da lista dos a visitar
+                        colors[currentV] = "Black"; // Marca o vertice como fechado
+                        break;
+                    }
+                    
+                }
+                
+                
+            }
+            
+        }
+        
+    }
+    
 }
 
 int main(){
