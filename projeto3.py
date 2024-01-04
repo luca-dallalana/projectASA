b69eac63a23739ae81f2c250987799dff7de52d7
luca-dallalana <luca.dallalana@tecnico.ulisboa.pt>
2024-01-04 14:10:13 +0000

    make

--- projeto3.py
+++ projeto3.py
@@ -13,21 +13,24 @@ infoPacotes = []
 for i in range(nPacotes):
     produto1, produto2, produto3, lucroPacote = [int(entrada) for entrada in input().split()]
     pacotes = LpVariable(f"pacote{i}", 0, None, LpInteger)
-    infoPacotes.append([pacotes, infoBrinquedos[produto1 - 1], infoBrinquedos[produto2 - 1], infoBrinquedos[produto3 - 1], lucroPacote])
+    infoPacotes.append([pacotes, infoBrinquedos[produto1 - 1][0], infoBrinquedos[produto2 - 1][0], infoBrinquedos[produto3 - 1][0], lucroPacote])
+    # Adicionar no info brinquedos os pacotes que contem o dito brinquedo
+
+# Restricoes
+for b in range(brinquedosDiferentes): # Maior numero que pode ser construido de brinquedos b
+    # Iterar a partir do 3 indice de info brinquedos ate ao fim para ter os pacotes que ele possui
+    pacotesQueTemBrinquedos = []
+    for p in range(nPacotes):
+        if infoBrinquedos[b][0] in (infoPacotes[p][1], infoPacotes[p][2],infoPacotes[p][3]):
+            pacotesQueTemBrinquedos.append(infoPacotes[p][0])
+    prob += infoBrinquedos[b][0] + lpSum(pacotesQueTemBrinquedos[i] for i in range(len(pacotesQueTemBrinquedos)))<= infoBrinquedos[b][2]
+
+# Restricao para o numero total de brinquedos
+prob += lpSum(infoBrinquedos[b][0] for b in range(brinquedosDiferentes)) + lpSum(3 * infoPacotes[p][0] for p in range(nPacotes)) <= maxBrinquedos
 
 # Funcao Objetivo
 prob += lpSum(infoBrinquedos[b][0] * infoBrinquedos[b][1] for b in range(brinquedosDiferentes)) + \
     lpSum(infoPacotes[p][0] * infoPacotes[p][4] for p in range(nPacotes))
-
-# Restricoes
-for b in range(brinquedosDiferentes): # Maior numero que pode ser construido de brinquedos b
-    prob += lpSum(infoBrinquedos[b][0]) <= infoBrinquedos[b][2]
-
-for p in range(nPacotes): # Limitar a producao de pacotes para o menor numero de brinquedos possiveis entre os 3 do pacote
-    minimo = min(min(infoPacotes[p][1], infoPacotes[p][2]), infoPacotes[i][3])
-    prob += lpSum(infoPacotes[p][0]) <= minimo
-
-prob += lpSum(infoBrinquedos[b][0] for b in range(brinquedosDiferentes)) + lpSum(3 * infoPacotes[p][0] for p in range(nPacotes)) <= maxBrinquedos
 
 prob.solve(GLPK(msg=0))
 print("Status:", LpStatus[prob.status])
